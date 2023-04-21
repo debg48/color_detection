3ef54f2b8a9fc0dad7f75ee7faf8c96f6c173478
debg48 <debgandhar4000@gmail.com>
2023-04-21 17:52:14 +0530

    video capture code

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-_,frame = cap.read()
+while True: 
+    _,frame = cap.read()
 
-cv2.imshow("Frame",frame)
-cv2.waitKey(0)
+    cv2.imshow("Frame",frame)
+    key = cv2.waitKey(1)
+
+    if key == 27 :
+        break
+
+cap.release()
+cv2.destroyAllWindows()
