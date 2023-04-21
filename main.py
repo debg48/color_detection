d57e4c3d6e3103b97e5ad0413527a568e6dcd4ef
debg48 <debgandhar4000@gmail.com>
2023-04-21 19:44:57 +0530

    color picker and color detector added

--- main.py
+++ main.py
@@ -23,8 +23,36 @@ while True:
     #pick pixel value
 
     pixel_center = hsv_frame[cy,cx]
-    print(pixel_center)
-    cv2.circle(frame,(cx,cy),5,(225,0,0),3)
+    hue_value=pixel_center[0]
+    s_value=pixel_center[1]
+    v_value=pixel_center[2]
+
+    # identifying the color based on hue
+
+    color = "UNDEFINED"
+    if hue_value < 5 :
+        color = "RED"
+    elif hue_value < 22 :
+        color = "ORANGE"
+    elif hue_value < 34 :
+        color = "YELLOW" 
+    elif hue_value < 80 :
+        color = "GREEN"
+    elif hue_value < 130 :
+        color = "BLUE"
+    elif hue_value < 143 :
+        color = "VIOLET"
+    elif hue_value < 168 :
+        color = "PINK"
+    else :
+        color="RED"
+        
+
+    pixel_center_bgr=frame[cy,cx]
+    b,g,r = int(pixel_center_bgr[0]),int(pixel_center_bgr[1]),int(pixel_center_bgr[2])
+
+    cv2.putText(frame,color,(10,70),0,1.5,(b,g,r),2)
+    cv2.circle(frame,(cx,cy),5,(25,25,25),3)
 
     cv2.imshow("Frame",frame)
     key = cv2.waitKey(1)
